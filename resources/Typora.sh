f12843e6803845f4aada4e47e095b660bd4c9014
hao <454947233@qq.com>
2020-05-31 20:10:00 +0800

    Typora

--- resources/Typora.sh
+++ resources/Typora.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#! /bin/bash
 # 上面中的 #! 是一种约定标记, 它可以告诉系统这个脚本需要什么样的解释器来执行;
 cd /Users/hao/programming/笔记
 
@@ -6,6 +6,7 @@ git add .
 
 git commit -m  "Typora"
 
-git push origin master
+#git push origin master
+git push
 
 echo -e `date '+%Y-%m-%d %H:%M:%S'` "完成笔记github备份" >> /Users/hao/programming/log/notes.log
