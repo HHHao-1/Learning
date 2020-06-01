e98ee420a082e4fc9fbf331c5ca0c099238a0a71
hao <454947233@qq.com>
2020-06-01 17:00:00 +0800

    提交的时间是: Mon Jun  1 17:00:00 CST 2020

--- resources/Typora.sh
+++ resources/Typora.sh
@@ -2,11 +2,13 @@
 # 上面中的 #! 是一种约定标记, 它可以告诉系统这个脚本需要什么样的解释器来执行;
 cd /Users/hao/programming/笔记
 
+time=`date`
+
 git add .
 
-git commit -m  "Typora"
+git commit -m  "提交的时间是: $time"
 
 #git push origin master
 git push https://HHHao-1:chzaizg556677@github.com/HHHao-1/Notes.git master
 
-echo -e `date '+%Y-%m-%d %H:%M:%S'` "完成笔记github备份" >> /Users/hao/programming/log/notes.log
+echo -e `date '+%Y-%m-%d %H:%M:%S'` "提交笔记" >> /Users/hao/programming/log/notes.log
