48f2b32a16b85551dd30f8f47d556dbf5463dcd0
chenghao <454947233@qq.com>
2020-11-13 11:33:01 +0800

    2020/11/13 11:33:00

--- 资料记录/笔记相关/定时任务脚本.sh
+++ 资料记录/笔记相关/定时任务脚本.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # 上面中的 #! 是一种约定标记, 它可以告诉系统这个脚本需要什么样的解释器来执行;
-cd /Users/chenghao/Nutstore\ Files/我的坚果云/01_笔记
+cd "/Users/chenghao/Nutstore Files/我的坚果云/01_笔记"
 
 time=`date '+%Y/%m/%d %T'`
 
